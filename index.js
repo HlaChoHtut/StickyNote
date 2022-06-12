c3b622d88003455b3a26d81d9e1211db2d710194
hchtut <hch.htut.yours@gmail.com>
2022-06-12 15:11:24 +0630

    Trying to switch

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 let today = document.getElementById('date');
+let noteName = document.getElementById('note');
+const through = document.getElementById('delete');
 var date = new Date();
 var day = date.getDate(); //day (1-31)
 var month = date.toLocaleString('default', { month: 'short' }); //months (0-11)
 var year = date.getFullYear();
 
 today.textContent = day + ' ' + month + ' ' + year;
+through.addEventListener('click', () => {
+	if (noteName.style.textDecoration !== 'line-through') {
+		noteName.style.textDecoration = 'line-through';
+	} else {
+		noteName.style.textDecoration !== 'line-through';
+	}
+});
+
